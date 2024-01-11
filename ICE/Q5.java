65347f593decb4f1446aeaee25e7dc156dcb735c
Keith <keithloh99@gmail.com>
2024-01-12 02:19:30 +0800

    feat: did qn 5 and start of 7

--- ICE/Q5.java
+++ ICE/Q5.java
@@ -7,56 +7,56 @@ public class Q5 {
     int size = sc.nextInt();
     alphabetSize(size);
     numberPattern(size);
-    stairPattern(size);
+    int sizeCopyC = size;
+    pyramid(sizeCopyC, 1);
     sc.close();
   }
 
   public static void alphabetSize(int size) {
     for (int i = 0; i < size; i++) {
-        char ch = (char) ('a' + i % 26);
-        System.out.print(ch + " ");
+      char ch = (char) ('a' + i % 26);
+      System.out.print(ch + " ");
     }
     System.out.println();
   }
 
-  public static void numberPattern(int size){
+  public static void numberPattern(int size) {
     String result = "";
     int init = 1;
-    for (int i = 0; i < size; i++ ){
+    boolean up = true;
+    for (int i = 0; i < size; i++) {
       int tempNum = init;
-      // ? Check what is the edge case for question 5
-      String temp = String.valueOf(tempNum) + String.valueOf(++tempNum) + String.valueOf(++tempNum);
-      init++;
+      String temp = "";
+      // * Handling when init below 7
+      if (up) {
+        temp = String.valueOf(tempNum) + String.valueOf(++tempNum) + String.valueOf(++tempNum);
+        init++;
+      } else {
+        // * Handle when init above 7
+        temp = String.valueOf(tempNum) + String.valueOf(--tempNum) + String.valueOf(--tempNum);
+        init--;
+      }
+      if (init > 7) {
+        up = false;
+      } else if (init == 1) {
+        up = true;
+      }
       // String temp = tempNum + "" + ++tempNum + "" + ++tempNum + "";
       result += temp + " ";
     }
     System.out.println(result);
   }
 
-  public static void stairPattern(int size){
-  String str = "";
-  for (int i = 1; i <= size; i++){
-    str += i;
-    System.out.println(str);
-  }
-}
+  public static void pyramid(int n, int i) {
+    if (n == 0)
+      return;
 
-  // ! Method using StringBuilder Method
-  // public static void stairPattern(int size){
-  //   StringBuilder sb = new StringBuilder();
-  //   for (int i = 1; i <= size; i++){
-  //     sb.append(i);
-  //     System.out.println(sb.toString());
-  //   }
-  // }
-  // ! Method using double for loop *incorrect*
-  // public static void stairPattern(int size){
-  //   for (int i = 1; i <= size; i ++ ){
-  //     String temp = "";
-  //     for (int j = 1; j <= i; j++){
-  //       temp += Integer.toString(j);
-  //     }
-  //     System.out.println(temp);
-  //   }
-  // }
+    int j;
+    for (j = 1; j < i; j++) {
+      System.out.print(j + " ");
+    }
+    System.out.println(j);
+
+    pyramid(n - 1, i + 1);
+  }
 }
--- ICE/Q7.java
+++ ICE/Q7.java
@@ -1,15 +1,81 @@
 public class Q7 {
     public static void main(String[] args) {
-  
+
         String[] days = {
-                "first", "second", "third", "fourth", 
-                "fifth", "sixth", "seventh", "eighth", 
+                "first", "second", "third", "fourth",
+                "fifth", "sixth", "seventh", "eighth",
                 "ninth", "tenth", "eleventh", "twelfth"
-                };
-        
+        };
+
         String day = days[0];
         switch (day) {
-            
-        } 
+            case "first":
+                System.out.println("On the first day of Christmas, my true love sent to me\n" +
+                        "A partridge in a pear tree.");
+            case "second":
+                System.out.println("On the second day of Christmas, my true love sent to me\n" +
+                        "" +
+                        "Two turtle doves\n" +
+                        "and a partridge in a pear tree.");
+            case "third":
+                System.out.println("On the third day of Christmas, my true love sent to me\n" +
+                        "Three French hens, two turtle doves\n" +
+                        "And a partridge in a pear tree.");
+            case "fourth":
+                System.out.println("On the fourth day of Christmas, my true love sent to me\n"
+                        + "Four calling birds, three French hens, two turtle doves\n"
+                        + "And a partridge in a pear tree.");
+            case "fifth":
+                System.out.println("On the fifth day of Christmas, my true love sent to me\n" +
+                        "Five golden rings.\n" +
+                        "Four calling birds, three French hens, two turtle doves\n" +
+                        "And a partridge in a pear tree.");
+            case "sixth":
+                System.out.println("On the sixth day of Christmas, my true love gave to me\n" +
+                        "Six geese a-laying,\n" +
+                        "Five golden rings.\n" +
+                        "Four calling birds, three French hens, two turtle doves\n" +
+                        "And a partridge in a pear tree.");
+            case "seventh":
+                System.out.println("On the seventh day of Christmas, my true love gave to me\n" +
+                        "Seven swans a-swimming, six geese a-laying,\n" +
+                        "Five golden rings.\n" +
+                        "Four calling birds, three French hens, two turtle doves\n" +
+                        "And a partridge in a pear tree.");
+            case "eighth":
+                System.out.println("On the eighth day of Christmas, my true love gave to me\n" +
+                        "Eight maids a-milking, seven swans a-swimming, six geese a-laying,\n" +
+                        "Five golden rings.\n" +
+                        "Four calling birds, three French hens, two turtle doves\n" +
+                        "And a partridge in a pear tree.");
+            case "ninth":
+                System.out.println("On the ninth day of Christmas, my true love gave to me\n" +
+                        "Nine ladies dancing, eight maids a-milking, seven swans a-swimming, six geese a-laying,\n" +
+                        "Five golden rings.\n" +
+                        "Four calling birds, three French hens, two turtle doves\n" +
+                        "And a partridge in a pear tree.");
+            case "tenth":
+                System.out.println("On the tenth day of Christmas, my true love gave to me\n" +
+                        "Ten lords a-leaping, nine ladies dancing, eight maids a-milking, seven swans a-swi'mmi'ng, six geese a-laying,\n"
+                        +
+                        "Five golden rings.\n" +
+                        "Fou'r calling birds, three French hens, two turtle doves\n" +
+                        "And a partridge in a pear tree.");
+            case "eleventh":
+                System.out.println("On the eleventh day of Christmas, my true love gave to me\n" +
+                        "Eleven pipers piping, ten lords a-leaping, nine ladies dancing, eight maids a-milking, seven swans a-swimming,\n"
+                        + "Six geese a-laying,\n" +
+                        "Five golden rings.\n" +
+                        "Four calling birds, three French hens, two turtle doves And a partridge in a pear tree.");
+            case "twelfth":
+                System.out.println("On the twelfth day of Christmas, my true love gave to me\n" +
+                        "Twelve drummers drumming, eleven pipers piping, ten lords a-leaping, nine ladies dancing, eight maids a-milking, seven swans a-swimming, six geese a-laying,\n"
+                        +
+                        "Five golden rings.\n" +
+                        "Four calling birds, three French hens, two turtle doves\n" +
+                        "And a partridge in a pear tree.");
+            default:
+                break;
         }
     }
+}
