33f4d33a1a0623b685751f5d74b8b5095438132b
Keith <keithloh99@gmail.com>
2024-02-11 01:33:34 +0800

    feat: add q1-4

--- ICE5/Q3/ShapeUtility.java
+++ ICE5/Q3/ShapeUtility.java
@@ -1,26 +1,72 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ShapeUtility {
-  
-  public Octagon getLargestOctagon(ArrayList<Shape> shapeList){
-    // Octagon largestOctagon;
-    // for ( Octagon tempOctagon: shapeList){
-    
-    if (shapeList == null || shapeList.size() == 0){
+
+  public static Octagon getLargestOctagon(ArrayList<Shape> shapeList) {
+    Octagon largestOctagon = null;
+    double largestArea = 0.0;
+    if (shapeList == null || shapeList.size() == 0) {
       return null;
     }
-
-    Shape largestShape;
-    
+    for (Shape tempShape : shapeList) {
+      if (tempShape instanceof Octagon) {
+        double tempArea = tempShape.getArea();
+        if (tempArea > largestArea) {
+          Octagon tempOctagon = (Octagon) tempShape;
+          largestOctagon = tempOctagon;
+          largestArea = tempArea;
+        }
+      }
+    }
+    return largestOctagon;
   }
 
-  public ArrayList<Shape> getAllNonRectangles(ArrayList<Shape> shapeList){
-    return 
+  public static ArrayList<Shape> getAllNonRectangles(ArrayList<Shape> shapeList) {
+    ArrayList<Shape> notRectangleArray = new ArrayList<Shape>();
+
+    if (shapeList == null) {
+      return notRectangleArray;
+    }
+
+    for (Shape tempShape : shapeList) {
+      if (!(tempShape instanceof Rectangle)) {
+        notRectangleArray.add(tempShape);
+      }
+    }
+    return notRectangleArray;
   }
 
-  public ArrayList<Square> getAllSquareWithMinArea(ArrayList<Shape> shapeList, int minArea){
-
+  public static ArrayList<Square> getAllSquareWithMinArea(ArrayList<Shape> shapeList,
+      int minArea) {
+    ArrayList<Square> squareWithMinArea = new ArrayList<Square>();
+    if (shapeList == null) {
+      return squareWithMinArea;
+    }
+    for (Shape tempShape : shapeList) {
+      if (tempShape instanceof Square) {
+        Square tempSquare = (Square) tempShape;
+        if (tempSquare.getArea() > minArea) {
+          squareWithMinArea.add(tempSquare);
+        }
+      }
+    }
+    return squareWithMinArea;
   }
 
-  public ArrayList<Shape> getAllRectangleWithMinLength(ArrayList<Shape> shapeList, int length){
-
+  public static ArrayList<Rectangle> getAllRectangleWithMinLength(ArrayList<Shape> shapeList, int minLength) {
+    ArrayList<Rectangle> rectangleWithMinLength = new ArrayList<Rectangle>();
+    if (shapeList == null) {
+      return rectangleWithMinLength;
+    }
+    for (Shape tempShape : shapeList) {
+      if (tempShape instanceof Rectangle) {
+        Rectangle tempRectangle = (Rectangle) tempShape;
+        if (tempRectangle.getLength() > minLength) {
+          rectangleWithMinLength.add(tempRectangle);
+        }
+      }
+    }
+    return rectangleWithMinLength;
   }
 }
---
+++ ICE5/Q5/ShapeAreaComparator.java
@@ -0,0 +1,3 @@
+public class ShapeAreaComparator implements Comparator{
+    
+}
