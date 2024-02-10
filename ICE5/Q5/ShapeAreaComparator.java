80fd123d5c855f1714f6264d6df410212e8f4c8a
Keith <keithloh99@gmail.com>
2024-02-11 01:58:12 +0800

    feat: q5 ice5

--- ICE5/Q5/ShapeAreaComparator.java
+++ ICE5/Q5/ShapeAreaComparator.java
@@ -1,3 +1,12 @@
-public class ShapeAreaComparator implements Comparator{
-    
+import java.util.*;
+
+public class ShapeAreaComparator implements Comparator<Shape> {
+    public int compare(Shape o1, Shape o2) {
+        if (o1.getArea() < o2.getArea())
+            return -1;
+        else if (o1.getArea() == o2.getArea())
+            return 0;
+        else
+            return 1;
+    }
 }
