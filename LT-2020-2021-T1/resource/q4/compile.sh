fdd8fb57f404e8a1ef832bf558e7e6cfaed0d071
Keith <keithloh99@gmail.com>
2024-03-03 14:26:39 +0800

    feat: cleanup LT

--- LT-2020-2021-T1/resource/q4/compile.sh
+++
@@ -1,3 +0,0 @@
-# javac -d generated -cp "lib/*:conf/*:" src/*
-# javac -d generated -cp "conf/*:lib/log4j/*:lib/thymeleaf/*" src/*
-javac -proc:none -d generated -cp "conf:lib/log4j/*:lib/thymeleaf/*:src" src/app/Example.java
