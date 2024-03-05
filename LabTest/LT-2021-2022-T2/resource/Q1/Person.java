db48a8b1a63eae7dfa4057d0de1eeb6c68da81ab
Keith <keithloh99@gmail.com>
2024-03-05 14:07:09 +0800

    feat: LT-2021-22-T2 - Q1

--- LabTest/LT-2021-2022-T2/resource/Q1/Person.java
+++ LabTest/LT-2021-2022-T2/resource/Q1/Person.java
@@ -3,6 +3,38 @@
  * Name: Keith Loh
  */
 
+import java.util.List;
+
 public class Person {
-   
+    private String name;
+    private int age;
+    private List<String> medicalCondition;
+    private String vaccineCentre;
+    private String vaccine;
+    private char shotType;
+
+    public Person(String name, int age, List<String> medicalCondition, String vaccineCentre, String vaccine,
+            char shotType) {
+        this.name = name;
+        this.age = age;
+        this.medicalCondition = medicalCondition;
+        this.vaccineCentre = vaccineCentre;
+        this.vaccine = vaccine;
+        this.shotType = shotType;
+    }
+
+    @Override
+    public String toString() {
+
+        return "name=" + name + ", age=" + age + ", medicalCondition=" + medicalCondition + ", shotType="
+                + shotType;
+    }
+
+    public int compareTo(Person anotherPerson) {
+        char shotType1 = shotType;
+        char shotType2 = shotType;
+        
+
+        return 0;
+    }
 }
