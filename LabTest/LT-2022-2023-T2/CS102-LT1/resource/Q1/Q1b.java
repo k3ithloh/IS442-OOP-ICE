b0e486a2d9f93f782bd53d01f0ff8b3b24989693
Keith <keithloh99@gmail.com>
2024-03-06 02:50:32 +0800

    feat: 2022-2023-T2-LT1 Q1B and C

--- LabTest/LT-2022-2023-T2/CS102-LT1/resource/Q1/Q1b.java
+++ LabTest/LT-2022-2023-T2/CS102-LT1/resource/Q1/Q1b.java
@@ -10,11 +10,46 @@ public class Q1b {
     /*
      * Write the method getLongestPalindromeWord
      */
+
+    static boolean isPalindrome(String str) {
+
+        // Pointers pointing to the beginning
+        // and the end of the string
+        int i = 0, j = str.length() - 1;
+
+        // While there are characters to compare
+        while (i < j) {
+
+            // If there is a mismatch
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+
+            // Increment first pointer and
+            // decrement the other
+            i++;
+            j--;
+        }
+
+        // Given string is a palindrome
+        return true;
+    }
+
     public static String getLongestPalindromeWord(ArrayList<String> inputs) {
         // insert your code here.
-
-        return null;
-         // to make this code compile. Please modify accordingly!
+        String longestPlaindrome = null;
+        for (String input : inputs) {
+            if (isPalindrome(input.toLowerCase())) {
+                if (longestPlaindrome != null) {
+                    if (input.length() > longestPlaindrome.length()) {
+                        longestPlaindrome = input;
+                    }
+                } else {
+                    longestPlaindrome = input;
+                }
+            }
+        }
+        return longestPlaindrome;
+        // to make this code compile. Please modify accordingly!
     }
 
     public static void main(String[] args) {
--- LabTest/LT-2022-2023-T2/CS102-LT1/resource/Q1/Q1c.java
+++ LabTest/LT-2022-2023-T2/CS102-LT1/resource/Q1/Q1c.java
@@ -1,6 +1,6 @@
 /*
- * Name:
- * Email ID:
+ * Name: Keith Loh
+ * Email ID: keith.loh.2021
  */
 
 import java.util.*;
@@ -10,14 +10,43 @@ public class Q1c {
      * Write the method getMostCommonVowel
      */
     public static ArrayList<Character> getMostCommonVowel(ArrayList<String> inputs) {
+        // ! ANSWER IS STILL WRONG
         // insert your code here.
+        HashMap<Character, Integer> vowelCounts = new HashMap<>();
+        for (char vowel : new char[] { 'a', 'e', 'i', 'o', 'u' }) {
+            vowelCounts.put(vowel, 0);
+        }
 
-        return null;
-         // to make this code compile. Please modify accordingly!
+        for (String word : inputs) {
+            HashSet<Character> uniqueVowels = new HashSet<>();
+            for (char ch : word.toLowerCase().toCharArray()) {
+                if (vowelCounts.containsKey(ch)) {
+                    uniqueVowels.add(ch);
+                }
+            }
+            for (char vowel : uniqueVowels) {
+                vowelCounts.put(vowel, vowelCounts.get(vowel) + 1);
+            }
+        }
+
+        int maxCount = Collections.max(vowelCounts.values());
+        ArrayList<Character> result = new ArrayList<>();
+        for (Map.Entry<Character, Integer> entry : vowelCounts.entrySet()) {
+            if (entry.getValue() == maxCount) {
+                result.add(entry.getKey());
+            }
+        }
+
+        if (result.isEmpty()) {
+            return new ArrayList<>(); // Return an empty list if no vowels found
+        }
+
+        return result;
+        // to make this code compile. Please modify accordingly!
     }
 
     public static void main(String[] args) {
-    
+
         int tcNum = 1;
         {
             ArrayList<String> inputs = new ArrayList<>(List.of("Abate", "Facet", "Pen", "idiom"));
