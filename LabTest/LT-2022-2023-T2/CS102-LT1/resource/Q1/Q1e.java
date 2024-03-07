f23473232a0e9e9fd082346ce428cce0220780b2
KeithLoh <keithloh99@gmail.com>
2024-03-07 18:55:20 +0800

    feat: q1-e LT

--- LabTest/LT-2022-2023-T2/CS102-LT1/resource/Q1/Q1e.java
+++ LabTest/LT-2022-2023-T2/CS102-LT1/resource/Q1/Q1e.java
@@ -4,12 +4,120 @@ public class Q1e {
     /*
      * Write the method getTermGPA
      */
-    public static double getTermGPA(String input) {
-        // insert your code here.
 
-        return 0.0;
-        // to make this code compile. Please modify accordingly!
+    
+    public static boolean isDigitOrDot(char ch) {
+        return (ch >= '0' && ch <= '9') || ch == '.';
     }
+
+    public static double getGradePoint(String input){
+        HashMap<String, Double> gradeMap = new HashMap<String, Double>();
+        gradeMap.put("A+", 4.3);
+        gradeMap.put("A",4.0);
+        gradeMap.put("A-",3.7);
+        gradeMap.put("B+",3.3);
+        gradeMap.put("B",3.0);
+        gradeMap.put("B-",2.7);
+        gradeMap.put("C+",2.3);
+        gradeMap.put("C",2.0);
+        gradeMap.put("C-",1.7);
+        gradeMap.put("D+",1.3);
+        gradeMap.put("D",1.0);
+        gradeMap.put("F",0.0);
+        // return gradeMap.get(input);
+        Double gradePoint = gradeMap.get(input);
+        if (gradePoint != null) {
+            return gradePoint;
+        } else {
+            return 0.0; // Default value
+        }
+    }
+    
+    public static double getTermGPA(String input) {
+    List<Double> pointUnits = getRawValues(input);
+    double sum = 0;
+    double numUnits = 0;
+
+    for (int i = 0; i < pointUnits.size(); i += 2) {
+        sum += pointUnits.get(i) * pointUnits.get(i + 1);
+        numUnits += pointUnits.get(i);
+    }
+
+    if (numUnits == 0) {
+        return 0.0; // Avoid division by zero
+    }
+
+    return sum / numUnits;
+}
+
+private static List<Double> getRawValues(String termGrades) {
+    int len = termGrades.length();
+    termGrades = termGrades + ' ';
+
+    List<Double> result = new ArrayList<>();
+    int idx = 0;
+    while (idx < len) {
+        String gradeLetter = "" + termGrades.charAt(idx++);
+        char lookAhead = termGrades.charAt(idx);
+        if (lookAhead == '+' || lookAhead == '-') {
+            gradeLetter += lookAhead;
+            idx++;
+        }
+        lookAhead = termGrades.charAt(idx);
+        if (isDigitOrDot(lookAhead)) {
+            String number = "";
+            while (isDigitOrDot(lookAhead)) {
+                number +=  lookAhead;
+                lookAhead = termGrades.charAt(++idx);
+            }
+            result.add(Double.parseDouble(number));
+        } else {
+            result.add(1.0);
+        }
+        result.add(getGradePoint(gradeLetter));
+    }
+    return result;
+}
+
+    // public static double getTermGPA(String input) {
+    //     // insert your code here.
+    //     // ArrayList<Double> totalGrades = new ArrayList<>();
+    //     double totalGrades = 0.0;
+    //     double totalUnits = 0.0;
+
+    //     int inputLength = input.length() - 1 ;
+    //     int i = 0;
+        
+    //     while (i < inputLength){
+    //         char tempChar = input.charAt(i);
+    //         char lookAhead = input.charAt(i + 1);
+    //         String grade = String.valueOf(tempChar);
+    //         if (lookAhead == '+' || lookAhead == '-'){
+    //             grade += lookAhead;
+    //             i ++;
+    //         }
+    //         i ++;
+
+    //         double tempUnits = 1.0;
+    //         if (i < inputLength && isDigitOrDot(input.charAt(i))) {
+    //         StringBuilder unitsBuilder = new StringBuilder();
+    //             while (i < inputLength && isDigitOrDot(input.charAt(i))) {
+    //                 unitsBuilder.append(input.charAt(i));
+    //                 i++;
+    //             }
+    //             tempUnits = Double.parseDouble(unitsBuilder.toString());
+    //         } else {
+    //             tempUnits = 1.0;
+    //         }
+    //         totalGrades += getGradePoint(grade) * tempUnits;
+    //         totalUnits += tempUnits;
+    // }
+    //     if (totalUnits == 0.0) {
+    //         return 0.0;
+    //     }
+    
+    // return totalGrades / totalUnits;
+    // }
     
     public static void main(String[] args) {
         int tcNum = 1;
